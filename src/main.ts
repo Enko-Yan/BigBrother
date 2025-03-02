95a44ab914bad27d45f91155ddd13e1b0d99b9e2
Enk0-A <sergiyenkoyan@gmail.com>
2025-03-02 15:54:23 -0600

    Queries added to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,32 @@ import inquirer from "inquirer";
 import { pool, dbConnection } from './connection';
 
 const getDepartments = async () => {
-    const qeury = `SELECT * FROM department;`
+    const departmentQuery = `SELECT * FROM department;`;
+    try {
+        const res = await pool.query(departmentQuery);
+        console.log(res.rows);
+    } catch (err) {
+        console.error("Error executing departmentQuery", err);
+    }
 }
+
+const getRoles = async () => {
+    const roleQuery = `SELECT * FROM role;`;
+    try {
+        const res = await pool.query(roleQuery);
+        console.log(res.rows);
+    } catch (err) {
+        console.error("Error executing roleQuery", err);
+    }
+}
+
+const getEmployees = async () => {
+    const employeeQuery = `SELECT * FROM employee;`;
+    try {
+        const res = await pool.query(employeeQuery);
+        console.log(res.rows);
+    } catch (err) {
+        console.error("Error executing employeeQuery", err);
+    }
+}
+
